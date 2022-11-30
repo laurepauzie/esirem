6ee3a7e26984b2b6ea2a99e4c242d8feea54bd2e
WyderSevrin <sevrin.wyder@gmail.com>
2022-11-30 11:43:55 +0100

    Test de ma classe livre dans le main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <vector>
 #include "livre.h"
 
 int main()
 {
-	Date d(17,5,2015);
-	//std::cout << d.get_mois() << std::endl;
+	std::vector<std::string> emprunteur = {"lchapus","lpauzie"};
+	Date d(17,5,1892);
+	Livre l1("Les Misérables","Victor Hugo","Français","Roman",d,9780453009669,emprunteur);
+	std::cout << l1.get_titre() << std::endl;
 	return 0;
 }
