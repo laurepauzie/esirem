8843e4a2544afcf015604d56474361ea737c7296
WyderSevrin <sevrin.wyder@gmail.com>
2022-11-30 11:30:42 +0100

    ajout de la classe livre avec tout les getteurs

--- livre.cpp
+++ livre.cpp
@@ -1,6 +1,27 @@
 #include "livre.h"
+#include <vector>
 
+Livre::Livre(std::string titre,std::string auteur,std::string langue,std::string genre,Date date_publication,int ISBN,std::vector<std::string> liste_emprunteur) : _titre(titre), _auteur(auteur), _langue(langue), _genre(genre), _date_publication(date_publication), _ISBN(ISBN), _liste_emprunteur(liste_emprunteur){
+}
 
-Livre::Livre(std::string titre,std::string auteur,std::string langue,std::string genre,Date date_publication,int ISBN,std::vector<std::string> indentifiant){
-	
+std::string Livre::get_titre(){
+	return _titre;
+}
+std::string Livre::get_auteur(){
+	return _auteur;
+}
+std::string Livre::get_langue(){
+	return _langue;
+}
+std::string Livre::get_genre(){
+	return _genre;
+}
+Date Livre::get_date_publication(){
+	return _date_publication;
+}
+int Livre::get_ISBN(){
+	return _ISBN;
+}
+std::vector<std::string> Livre::get_liste_emprunteur(){
+	return _liste_emprunteur;
 }
--- livre.h
+++ livre.h
@@ -10,7 +10,14 @@ private:
 	std::string _genre;
 	Date _date_publication;
 	int _ISBN;
-	std::vector<std::string> _indentifiant;
+	std::vector<std::string> _liste_emprunteur;
 public:
-	Livre(std::string titre,std::string auteur,std::string langue,std::string genre,Date date_publication,int ISBN,std::vector<std::string> indentifiant);
+	Livre(std::string titre,std::string auteur,std::string langue,std::string genre,Date date_publication,int ISBN,std::vector<std::string> liste_emprunteur);
+	std::string get_titre();
+	std::string get_auteur();
+	std::string get_langue();
+	std::string get_genre();
+	Date get_date_publication();
+	int get_ISBN();
+	std::vector<std::string> get_liste_emprunteur();
 };
